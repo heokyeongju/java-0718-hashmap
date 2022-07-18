03a987143287f2903e261d8bcefc021cdd1a72d3
heokyeongju <beewt@naver.com>
2022-07-18 13:49:24 +0900

    generic class HashMap, put method test case

--- src/main/java/com/ll/exam/HashMap.java
+++ src/main/java/com/ll/exam/HashMap.java
@@ -2,4 +2,6 @@ package com.ll.exam;
 
 public class HashMap<K,V> {
 
+    public void put(K key, V value) {
+    }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -17,4 +17,9 @@ class HashMapTest {
     void 제너릭이_가능하다() {
         HashMap<String, Integer> map = new HashMap<>();
     }
+    @Test
+    void put() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+    }
 }

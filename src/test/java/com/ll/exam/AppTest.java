41cd771caf51c5e5f77b5c95f5a47c5bc2374629
heokyeongju <beewt@naver.com>
2022-07-18 13:47:27 +0900

    file name modified

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,20 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class AppTest {
+}
+
+class HashMapTest {
+    @Test
+    void 클래스가_존재한다() {
+        HashMap map = new HashMap();
+    }
+    @Test
+    void 제너릭이_가능하다() {
+        HashMap<String, Integer> map = new HashMap<>();
+    }
+}
